11f077e9f4b8a3dd84dcfce1d2dbdf9ce4615876
Everton Pereira <everton.mwp@gmail.com>
2019-08-28 19:41:44 -0300

    Add Typeorm and postgres

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'db',
+  port: 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'postgres',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { Logger } from '@nestjs/common'
+import { ApplicationConfig } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('bootstrap')
+  const port = process.env.PORT || 4000
+  const app = await NestFactory.create(ApplicationConfig)
+
+  app.enableCors()
+
+  await app.listen(port)
+
+  logger.log(`Application listening on port: ${port}`)
 }
-bootstrap();
+
+bootstrap()
